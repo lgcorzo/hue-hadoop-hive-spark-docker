b67f01ace2e110ec92bf3bfe9e9913415680bada
L.corzo <l.corzo@lantek.com>
2023-11-02 23:24:52 +0100

    set jupyter name and ports

--- jupyter/run.sh
+++ jupyter/run.sh
@@ -3,4 +3,4 @@
 echo "Starting jupyter notebook..."
 
 jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token='' --notebook-dir=/home/jupyter &
-sudo systemctl enable --now code-server@$USERNAME
+sudo systemctl enable --now code-server@jupyter
